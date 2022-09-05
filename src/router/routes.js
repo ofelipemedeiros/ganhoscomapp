96f34e37dbef823535f2c0d35fe877f71c563a6a
Felipe Medeiros <ofelipemedeiros@hotmail.com>
2022-09-05 15:54:08 -0300

    mais atualização de link quebrado

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,7 +8,7 @@ const routes = [
       { path: '', name: 'loginDefault', component: () => import('pages/PageLogin.vue') },
       { path: 'login', name: 'login', component: () => import('src/pages/PageLogin.vue') },
       { path: 'email-confirmation', name: 'email-confirmation', component: () => import('src/pages/PageEmailConfirmation.vue') },
-      { path: 'resetPassword', name: 'resetPassword', component: () => import('src/pages/PageResetPassword.vue') }
+      { path: 'resetPassword', name: 'resetPassword', component: () => import('src/pages/ResetPassword.vue') }
     ]
   },
   {
