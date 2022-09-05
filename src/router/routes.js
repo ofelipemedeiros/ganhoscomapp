fd7fdc2023db7338871e9c2f5e47ecbb4d13f8f5
Felipe Medeiros <ofelipemedeiros@hotmail.com>
2022-09-05 15:24:02 -0300

    arrumei o link do register

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,10 +7,16 @@ const routes = [
       //{ path: '', component: () => import('pages/IndexPage.vue') },
       { path: '', name: 'loginDefault', component: () => import('pages/PageLogin.vue') },
       { path: 'login', name: 'login', component: () => import('src/pages/PageLogin.vue') },
-      { path: 'register', name: 'register', component: () => import('src/pages/PageRegister.vue') },
       { path: 'email-confirmation', name: 'email-confirmation', component: () => import('src/pages/PageEmailConfirmation.vue') },
       { path: 'ForgotPassword', name: 'ForgotPassword', component: () => import('src/pages/PageForgotPassword.vue') },
       { path: 'resetPassword', name: 'resetPassword', component: () => import('src/pages/PageResetPassword.vue') }
+    ]
+  },
+  {
+    path: '/register',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: 'register', name: 'register', component: () => import('src/pages/PageRegister.vue') }
     ]
   },
   {
