4c7ab083df2d39512868d63b73632f2cbc3a6b91
Felipe Medeiros <ofelipemedeiros@hotmail.com>
2022-09-06 17:27:05 -0300

    modifiquei o logindefault

--- src/router/routes.js
+++ src/router/routes.js
@@ -5,7 +5,7 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       //{ path: '', component: () => import('pages/IndexPage.vue') },
-      { path: '', name: 'loginDefault', component: () => import('pages/PageLogin.vue') },
+      //{ path: '', name: 'loginDefault', component: () => import('pages/PageLogin.vue') },
       { path: 'login', name: 'login', component: () => import('src/pages/PageLogin.vue') },
       { path: 'email-confirmation', name: 'email-confirmation', component: () => import('src/pages/PageEmailConfirmation.vue') },
       { path: 'resetPassword', name: 'resetPassword', component: () => import('src/pages/ResetPassword.vue') }
@@ -29,7 +29,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/ProfileLogin.vue'),
     children: [
-      { path: 'me', name: 'me', component: () => import('pages/PageMe.vue') },
+      { path: 'loginDefault', name: 'loginDefault', component: () => import('pages/PageMe.vue') },
 
       //{ path: '', component: () => import('pages/Index.vue') }
     ],
